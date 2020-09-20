140ba9d0ac66dfe67720f3b83dca40e89c623ec4
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-20 14:20:00 +0530

    Updates

---
+++ precache-manifest.0459b6487bdd7b5bb6aa5417638e3e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdc30950d24757d25e338573246dc81",
+    "url": "/markdown-to-text1/index.html"
+  },
+  {
+    "revision": "0e85a6db61370102d391",
+    "url": "/markdown-to-text1/static/css/main.df070088.chunk.css"
+  },
+  {
+    "revision": "a1958a65fd28b344342e",
+    "url": "/markdown-to-text1/static/js/2.82206392.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-to-text1/static/js/2.82206392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e85a6db61370102d391",
+    "url": "/markdown-to-text1/static/js/main.e936ad67.chunk.js"
+  },
+  {
+    "revision": "3070b60b337ea753ac34",
+    "url": "/markdown-to-text1/static/js/runtime-main.6114b20b.js"
+  }
+]);
