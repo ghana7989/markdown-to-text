94eb8732b73610d0e46681ace425d9e646ccf044
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-20 13:07:55 +0530

    Final Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown'
 import './App.css';
 
 export default function App() {
+
+  const [markdown, setMarkdown] = useState(`
+  # On left YOU can write the markdown
+  # On right YOU can see the text
+  - Use - for unordered list
+
+  `);
   return (
     <div className="app">
-      <textarea />
+      <textarea id="text-area" onChange={(e) => setMarkdown(e.target.value)} value={markdown} />
 
-      <div className="preview" />
+      <ReactMarkdown source={markdown} className="preview" />
+      <button className="clear" onClick={() => {
+        setMarkdown("");
+        document.getElementById("text-area").focus()
+      }}>Clear</button>
     </div>
   );
 }
